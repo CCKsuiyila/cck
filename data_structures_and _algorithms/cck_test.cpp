329a529ca8fc85100fbc1033dae379d852490964
CCKsuiyila <1453787167@qq.com>
2018-04-03 01:03:50 +0800

    ..

---
+++ data_structures_and _algorithms/cck_test.cpp
@@ -0,0 +1,21 @@
+
+£include<iostream>
+using namespace std;
+int main(){
+    cout<<!"£$%^&*()@:{
+}}ISJSJ:wq
+
+})"
+}
+#include<iostream>
+using namespace std;
+include<iostream>
+using namespace std;
+int main(){
+	
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0)
+}
+
